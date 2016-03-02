7cae7e431a2e497029069b815b084ea1dcbb0158
Lee Hoyt <leehoyt@bellsouth.net>
2016-03-02 16:04:52 -0600

    Add visualizer

--- js/app.js
+++ js/app.js
@@ -1,14 +1,97 @@
+var initialHouses = [
+		{
 
-var ViewModel = [
-	'Albany',
-	'Springfield',
-	'Holden',
-	'Livingston',
-	'Satsuma',
-	'Walker',
-	'Denham Springs',
-	'Baton Rouge',
-	'Hammond'
-];
+			houseSrc: 'img/Visualizer/House_Green.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/green.png'
+		},
 
-ko.applyBindings(ViewModel);
+		{
+
+			houseSrc: 'img/Visualizer/House_Stone.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/stone.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Brown.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/brown.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Red.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/red.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Charcoal.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/charcoal.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_White.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/white.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Blue.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/blue.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Beige.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/beige.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Gray.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/gray.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Slate.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/slate.png'
+		},
+
+		{
+
+			houseSrc: 'img/Visualizer/House_Tan.jpg',
+			buttonSrc: 'img/Visualizer/Buttons/Tan.png'
+		}
+	];
+
+var House = function(data) {
+
+	this.houseSrc = ko.observable(data.houseSrc);
+	this.buttonSrc = ko.observable(data.buttonSrc);
+
+
+};
+
+var ViewModel = function() {
+	var self = this;
+
+	this.houseList = ko.observableArray([]);
+
+	initialHouses.forEach(function(houseItem) {
+		self.houseList.push( new House(houseItem));
+	});
+
+	this.currentHouse = ko.observable(this.houseList() [0] );
+
+	this.setHouse = function(clickedHouse) {
+		self.currentHouse(clickedHouse);
+	};
+
+	/*this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};*/
+}
+
+ko.applyBindings(new ViewModel());
---
+++ js/app_old.js
@@ -0,0 +1,30 @@
+
+var cityNames = [
+	'Albany',
+	'Springfield',
+	'Holden',
+	'Livingston',
+	'Satsuma',
+	'Walker',
+	'Denham Springs',
+	'Baton Rouge',
+	'Hammond'
+];
+
+var homePics = [
+	{imgSrc: 'img/Visualizer/House_Green.jpg'},
+	{imgSrc: 'img/Visualizer/House_Brown.jpg'},
+	{imgSrc: 'img/Visualizer/House_Stone.jpg'},
+	{imgSrc: 'img/Visualizer/House_Red.jpg'}
+];
+
+var roofColors = [
+	'img/Visualizer/Buttons/green.png',
+	'img/Visualizer/Buttons/stone.png',
+	'img/Visualizer/Buttons/red.png',
+	'img/Visualizer/Buttons/brown.png'
+];
+
+
+/*ko.applyBindings(cityNames);*/
+ko.applyBindings(roofColors);
